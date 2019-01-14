b38f61aff1546c64a668e7e77483598dde2e218f
Tal Kol <talkol@gmail.com>
2019-01-14 19:22:46 +0200

    Added cross compilation for more operating systems

--- build-binaries.sh
+++ build-binaries.sh
@@ -3,14 +3,27 @@ rm -rf ./_bin
 
 mkdir -p ./_bin
 
-# mac
-echo "\n\n*** MAC:"
-go build -o _bin/gamma-cli
-tar -zcvf ./_bin/gammacli-mac-v1.2.3.tar.gz ./_bin/gamma-cli
-openssl sha256 ./_bin/gammacli-mac-v1.2.3.tar.gz
+VERSION="v0.6.3"
 
-# linux
-echo "\n\n*** LINUX:"
-GOOS=linux go build -o _bin/gamma-cli
-tar -zcvf ./_bin/gammacli-linux-v1.2.3.tar.gz ./_bin/gamma-cli
-openssl sha256 ./_bin/gammacli-linux-v1.2.3.tar.gz
+echo "\n\n*** MAC:"
+GOOS=darwin GOARCH=amd64 go build -o _bin/gamma-cli
+tar -zcvf ./_bin/gammacli-mac-$VERSION.tar.gz ./_bin/gamma-cli
+
+echo "\n\n*** LINUX (x86-64):"
+GOOS=linux GOARCH=amd64 go build -o _bin/gamma-cli
+tar -zcvf ./_bin/gammacli-linux-x86-64-$VERSION.tar.gz ./_bin/gamma-cli
+
+echo "\n\n*** LINUX (i386):"
+GOOS=linux GOARCH=386 go build -o _bin/gamma-cli
+tar -zcvf ./_bin/gammacli-linux-i386-$VERSION.tar.gz ./_bin/gamma-cli
+
+echo "\n\n*** WINDOWS:"
+GOOS=windows GOARCH=386 go build -o _bin/gamma-cli.exe
+zip -r ./_bin/gammacli-windows-$VERSION.zip ./_bin/gamma-cli.exe
+
+cd ./_bin
+
+openssl sha256 gammacli-mac-$VERSION.tar.gz >> ./checksums.txt
+openssl sha256 gammacli-linux-x86-64-$VERSION.tar.gz >> ./checksums.txt
+openssl sha256 gammacli-linux-i386-$VERSION.tar.gz >> ./checksums.txt
+openssl sha256 gammacli-windows-$VERSION.zip >> ./checksums.txt
