1aa169ca45f44547f62bb82e80f6f42a0f4b217c
Tal Kol <talkol@gmail.com>
2019-01-14 19:39:26 +0200

    Slightly improved build script

--- build-binaries.sh
+++ build-binaries.sh
@@ -8,18 +8,22 @@ VERSION="v0.6.3"
 echo "\n\n*** MAC:"
 GOOS=darwin GOARCH=amd64 go build -o _bin/gamma-cli
 tar -zcvf ./_bin/gammacli-mac-$VERSION.tar.gz ./_bin/gamma-cli
+rm ./_bin/gamma-cli
 
 echo "\n\n*** LINUX (x86-64):"
 GOOS=linux GOARCH=amd64 go build -o _bin/gamma-cli
 tar -zcvf ./_bin/gammacli-linux-x86-64-$VERSION.tar.gz ./_bin/gamma-cli
+rm ./_bin/gamma-cli
 
 echo "\n\n*** LINUX (i386):"
 GOOS=linux GOARCH=386 go build -o _bin/gamma-cli
 tar -zcvf ./_bin/gammacli-linux-i386-$VERSION.tar.gz ./_bin/gamma-cli
+rm ./_bin/gamma-cli
 
 echo "\n\n*** WINDOWS:"
 GOOS=windows GOARCH=386 go build -o _bin/gamma-cli.exe
 zip -r ./_bin/gammacli-windows-$VERSION.zip ./_bin/gamma-cli.exe
+rm ./_bin/gamma-cli.exe
 
 cd ./_bin
 
