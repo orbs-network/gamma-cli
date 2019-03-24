fcb9ae62b7d01ccb8d27d0b7ae1808d3d570bcd4
Tal Kol <talkol@gmail.com>
2019-03-24 12:23:43 +0200

    Added copyright header to all sources

--- jsoncodec/config.go
+++ jsoncodec/config.go
@@ -1,3 +1,9 @@
+// Copyright 2019 the gamma-cli authors
+// This file is part of the gamma-cli library in the Orbs project.
+//
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+// The above notice should be included in all copies or substantial portions of the software.
+
 package jsoncodec
 
 import "encoding/json"
