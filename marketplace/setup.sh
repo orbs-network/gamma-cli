66ef9c8ded92c86243221e328098f753bb3fccf3
Itamar Arjuan <itamar.arjuan@gmail.com>
2019-05-27 11:36:30 +0300

    Update setup.sh

--- marketplace/setup.sh
+++ marketplace/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# Cleanup a gamma-server container incase there is one
+docker rm -fv orbs-gamma-server
+
 GAMMA_CLI_VERSION="v0.6.7"
 GAMMA_CLI_URL="https://github.com/orbs-network/gamma-cli/releases/download/$GAMMA_CLI_VERSION/gammacli-linux-x86-64-$GAMMA_CLI_VERSION.tar.gz"
 
