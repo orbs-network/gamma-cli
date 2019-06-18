4fc5490ace39d270f3011865064206375a68778c
Jonathan Levison <jonathan.levison@gmail.com>
2019-06-18 12:21:35 +0300

    version update

--- marketplace/setup.sh
+++ marketplace/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-# Cleanup a gamma-server container incase there is one
+# Cleanup a gamma-server container in case there is one
 docker rm -fv orbs-gamma-server orbs-prism
 docker pull orbsnetwork/gamma:v1.0.6
 
-GAMMA_CLI_VERSION="v0.7.0"
+GAMMA_CLI_VERSION="v0.7.1"
 GAMMA_CLI_URL="https://github.com/orbs-network/gamma-cli/releases/download/$GAMMA_CLI_VERSION/gammacli-linux-x86-64-$GAMMA_CLI_VERSION.tar.gz"
 
 echo "Downloading pre-built gamma-cli ($GAMMA_CLI_VERSION) from it's official GitHub release repository.."
