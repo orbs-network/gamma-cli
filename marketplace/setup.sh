1422ce6644fb118329cde61286fa3178b349e5a1
Jonathan Levison <jonathan.levison@gmail.com>
2019-06-26 11:43:43 +0300

    v0.8.0

--- marketplace/setup.sh
+++ marketplace/setup.sh
@@ -4,7 +4,7 @@
 docker rm -fv orbs-gamma-server orbs-prism
 docker pull orbsnetwork/gamma:v1.0.6
 
-GAMMA_CLI_VERSION="v0.7.1"
+GAMMA_CLI_VERSION="v0.8.0"
 GAMMA_CLI_URL="https://github.com/orbs-network/gamma-cli/releases/download/$GAMMA_CLI_VERSION/gammacli-linux-x86-64-$GAMMA_CLI_VERSION.tar.gz"
 
 echo "Downloading pre-built gamma-cli ($GAMMA_CLI_VERSION) from it's official GitHub release repository.."
