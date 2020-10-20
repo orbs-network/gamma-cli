978c8d29e9fe06add0727c55acc2baee0191ad7b
Kirill Maksimov <kirill@orbs.com>
2020-10-20 15:05:39 +0300

    Inject version from outside

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 )
 
-const GAMMA_CLI_VERSION = "0.10.0"
+var GAMMA_CLI_VERSION string
 const CONFIG_FILENAME = "orbs-gamma-config.json"
 const TEST_KEYS_FILENAME = "orbs-test-keys.json"
 const LOCAL_ENV_ID = "local"
